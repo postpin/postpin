77f6bc3c070ede34d34c62a3188b666df6f05cc1
Jaymee Liu <jaymeeliu@calvisitor-10-105-148-120.calvisitor.1918.berkeley.edu>
2018-01-23 20:38:21 -0800

    discover css

--- client/src/pages/Discover/Discover.js
+++ client/src/pages/Discover/Discover.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import "./Discover.css";
 
 class Discover extends Component {
 
   render() {
     return (
+
       <div className="discover">
         <br />
         <h1> Discover Page </h1>
