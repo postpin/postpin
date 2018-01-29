b864808cda2461e81e9f635fa3b72cc3b9936fe1
ulaiwa <stellaulw@gmail.com>
2018-01-28 19:37:34 -0800

    discover change

--- client/src/pages/Discover/Discover.js
+++ client/src/pages/Discover/Discover.js
@@ -1,18 +1,116 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import ReactDOM from "react-dom";
+import PhotoGrid from "react-photo-feed";
+import { Col, Row, Container } from "../../components/Grid";
+import "react-photo-feed/library/style.css";
 import "./Discover.css";
+import ImgList from '../../components/ImgList';
+import SearchForm from '../../components/SearchForm';
+import Jumbotron from "../../components/Jumbotron";
 
-class Discover extends Component {
+
+const demoPhotos = [
+  {
+    id : 1, src : "https://images.unsplash.com/photo-1504814532849-cff240bbc503?ixlib=rb-0.3.5&s=33eae9438f015e797df3fcf129401bc3&auto=format&fit=crop&w=3136&q=80",
+    bigSrc : "https://images.unsplash.com/photo-1504814532849-cff240bbc503?ixlib=rb-0.3.5&s=33eae9438f015e797df3fcf129401bc3&auto=format&fit=crop&w=3136&q=80"
+  },
+  {
+    id : 2, src : "https://images.unsplash.com/photo-1490365728022-deae76380607?ixlib=rb-0.3.5&s=cd0352c4c74192651e42f1949116e5fa&auto=format&fit=crop&w=1867&q=80",
+    bigSrc : "https://images.unsplash.com/photo-1490365728022-deae76380607?ixlib=rb-0.3.5&s=cd0352c4c74192651e42f1949116e5fa&auto=format&fit=crop&w=1867&q=80"
+  },
+  {
+    id : 3, src : "https://images.unsplash.com/photo-1504814532849-cff240bbc503?ixlib=rb-0.3.5&s=33eae9438f015e797df3fcf129401bc3&auto=format&fit=crop&w=3136&q=80",
+    bigSrc : "https://images.unsplash.com/photo-1504814532849-cff240bbc503?ixlib=rb-0.3.5&s=33eae9438f015e797df3fcf129401bc3&auto=format&fit=crop&w=3136&q=80"
+  },
+  {
+    id : 4, src : "https://images.unsplash.com/photo-1490365728022-deae76380607?ixlib=rb-0.3.5&s=cd0352c4c74192651e42f1949116e5fa&auto=format&fit=crop&w=1867&q=80",
+    bigSrc : "https://images.unsplash.com/photo-1490365728022-deae76380607?ixlib=rb-0.3.5&s=cd0352c4c74192651e42f1949116e5fa&auto=format&fit=crop&w=1867&q=80"
+  },
+  {
+    id : 5, src : "https://images.unsplash.com/photo-1504814532849-cff240bbc503?ixlib=rb-0.3.5&s=33eae9438f015e797df3fcf129401bc3&auto=format&fit=crop&w=3136&q=80",
+    bigSrc : "https://images.unsplash.com/photo-1504814532849-cff240bbc503?ixlib=rb-0.3.5&s=33eae9438f015e797df3fcf129401bc3&auto=format&fit=crop&w=3136&q=80"
+  },
+  {
+    id : 6, src : "https://images.unsplash.com/photo-1490365728022-deae76380607?ixlib=rb-0.3.5&s=cd0352c4c74192651e42f1949116e5fa&auto=format&fit=crop&w=1867&q=80",
+    bigSrc : "https://images.unsplash.com/photo-1490365728022-deae76380607?ixlib=rb-0.3.5&s=cd0352c4c74192651e42f1949116e5fa&auto=format&fit=crop&w=1867&q=80"
+  },
+  {
+    id : 7, src : "https://images.unsplash.com/photo-1504814532849-cff240bbc503?ixlib=rb-0.3.5&s=33eae9438f015e797df3fcf129401bc3&auto=format&fit=crop&w=3136&q=80",
+    bigSrc : "https://images.unsplash.com/photo-1504814532849-cff240bbc503?ixlib=rb-0.3.5&s=33eae9438f015e797df3fcf129401bc3&auto=format&fit=crop&w=3136&q=80"
+  }
+];
+
+class Discover extends React.Component {
+  constructor(props){
+    super(props);
+  }
 
   render() {
     return (
+      <Container fluid>
+        <Jumbotron>
+          <div className="jumbo-text">
+            <div className="headline">Caption it</div>
+            <div className="blah">Be inspirational</div>
+            <div className="blah">Be creative</div>
+            <div className="search-bar"><SearchForm onSearch={this.performSearch} /> </div>
+          </div>
+        </Jumbotron>
 
-      <div className="discover">
-        <br />
-        <h1> Discover pace </h1>
-      </div>
+      	<div>
+      		<PhotoGrid columns={6} photos={demoPhotos} />
+      	</div>
+      </Container>
     )
   }
 }
-
-
 export default Discover;
+
+
+// class Metrics extends Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			imgs: [],
+// 			loadingState: true
+// 		};
+// 	}
+//
+// 	componentDidMount() {
+// 		this.performSearch();
+// 	}
+//
+// 	performSearch = (query = 'lanscape') => {
+// 		axios
+// 			.get(
+// 				`https://api.unsplash.com/search/photos/?page=1&per_page=15&query=${query}&client_id=fa0b26bd4c0834d6a66f39587142bd9bac839fc21d640ed07a509347b37dbd5c`
+// 			)
+// 			.then(data => {
+// 				this.setState({ imgs: data.data.results, loadingState: false });
+// 			})
+// 			.catch(err => {
+// 				console.log('Error happened during fetching!', err);
+// 			});
+// 	};
+//
+//
+// 	render() {
+// 		return (
+// 			<div>
+// 				<div className="main-header">
+// 					<div className="inner">
+// 						<h1 className="main-title">ImageSearch</h1>
+// 						<SearchForm onSearch={this.performSearch} />
+// 					</div>
+// 				</div>
+// 				<div className="main-content">
+// 					{this.state.loadingState
+// 						? <p>Loading</p>
+// 						: <ImgList data={this.state.imgs} />}
+// 				</div>
+// 			</div>
+// 		);
+// 	}
+// }
+// export default Metrics;
--- client/src/pages/Post/Post.js
+++ client/src/pages/Post/Post.js
@@ -1,15 +1,111 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import SkyLight from "react-skylight";
+// import API from "../../routes/api/post";
+import { Input, FormBtn } from "../../components/Form";
+import { Col, Row, Container } from "../../components/Grid";
+import ReactDOM from "react-dom";
+// import deer from "./Post";
 
-class Post extends Component {
+// class LikeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
+//
+//   _onClick = e => {
+//     this.setState({ liked: true });
+//   }
+//
+//   render() {
+//     const label = this.state.liked ? 'Liked' : 'Like';
+//
+//     return <button onClick={this._onClick} disabled={this.state.liked}>{label}</button>;
+//   }
+// }
+
+// ReactDOM.render(
+//   <LikeButton />,
+//   document.querySelector('#main')
+// );
+
+
+class Post extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  // componentDidMount() {
+  // this.loadPosts();
+  // }
+
+//   handleFormSubmit = event => {
+//   event.preventDefault();
+//   if (this.state.postTitle && this.state.postDescription) {
+//     API.savePost({
+//       title: this.state.postTitle,
+//       discription: this.state.postDescription,
+//     })
+//       .then(res => this.loadPosts())
+//       .catch(err => console.log(err));
+//   }
+// };
 
   render() {
+
+    var postModalCSS = {
+      backgroundColor: '#FFFFEF',
+      color: 'black',
+      width: '70%',
+      height: '600px',
+      marginTop: '-300px',
+      marginLeft: '-35%',
+    };
+
     return (
-      <div className="post">
-        <h1> Post Page </h1>
+      <div>
+        <section>
+          <button onClick={() => this.customDialog.show()}>Open Modal</button>
+        </section>
+
+
+
+        <SkyLight dialogStyles={postModalCSS}
+        hideOnOverlayClicked ref={ref => this.customDialog = ref}
+        title="Caption this">
+
+        <Row>
+          <Col size="md-6">
+          <h1> picture render here </h1>
+
+          <button> download image </button>
+          <button> like </button>
+
+            </Col>
+            <Col size="md-6 sm-12">
+
+            <form>
+              <Input
+                // value={this.state.postTitle}
+                // onChange={this.handleInputChange}
+                name="comment"
+                placeholder="your comment here..."
+              />
+
+              <FormBtn
+                // disabled={!(this.state.postDescription && this.state.postTitle)}
+                onClick={this.handleFormSubmit}
+              >
+                post your comment
+              </FormBtn>
+              </form>
+            </Col>
+          </Row>
+        </SkyLight>
       </div>
     )
   }
 }
 
+Post.displayName = 'PostPage';
 
 export default Post;
