66b7929287e65e4efec4f1b42923bbc3a29d4b92
KevinLuc1 <kevinluc1993@gmail.com>
2018-02-01 19:56:11 -0800

    merging stella-discovery to main postpin

--- client/src/pages/Discover/Discover.js
+++ client/src/pages/Discover/Discover.js
@@ -1,19 +1,88 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import ReactDOM from "react-dom";
+import PhotoGrid from "react-photo-feed";
+import { Col, Row, Container } from "../../components/Grid";
+import "react-photo-feed/library/style.css";
 import "./Discover.css";
+import ImgList from '../../components/ImgList';
+import SearchForm from '../../components/SearchForm';
+import Jumbotron from "../../components/Jumbotron";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-class Discover extends Component {
+
+class Discover extends React.Component {
+  constructor() {
+		super();
+		this.state = {
+			imgs: [],
+      photos: [],
+			loadingState: true
+		};
+	}
+
+  componentDidMount() {
+		this.performSearch();
+
+	}
+
+	performSearch = (query = 'landscape') => {
+		axios
+			.get(
+				`https://api.unsplash.com/search/photos/?page=1&per_page=28&query=${query}&client_id=7c576b9235c5943df0c01ef4561602af3d56d65052d7bff4b207e6aee46b041e`
+			)
+			.then(response => {
+				// this.setState({ imgs: response.data.results, loadingState: false });
+        console.log('response.data.results: ', response.data.results);
+
+        const potatoes = []
+        if (!response.data.results || response.data.results.length === 0) {
+          return <div>Loading ... </div>
+        }
+        response.data.results.map(img => {
+          potatoes.push({
+            id: img.id,
+            src: img.urls.regular,
+            bigSrc: img.urls.regular
+          });
+        });
+        this.setState({imgs: potatoes})
+
+
+
+			})
+			.catch(err => {
+				console.log('Error happened during fetching!', err);
+        // <PhotoGrid columns={4} photos={this.renderContent()} />
+			});
+
+
+
+	};
+
+
+
   render() {
-    // this.props.setSignedIn();
-
     return (
+      <Container fluid>
+        <Jumbotron>
+          <div className="jumbo-text">
+            <div className="headline">Captionized</div>
+            <div className="blah">Be inspirational Be creative</div>
+            <div className="search-bar"><SearchForm onSearch={this.performSearch} /> </div>
+				<button className="comment"> comment </button>
+          </div>
+        </Jumbotron>
+      	<div>
+          {!this.state.loadingState ? <div>Loading ... </div>
+            :<PhotoGrid columns={4} photos={this.state.imgs} />}
 
-      <div className="discover">
-        <br />
-        <h1> Discover pace </h1>
-      </div>
+      	</div>
+        </Container>
+
+
+
     )
   }
 }
-
-
 export default Discover;
